24ae80e0da9dc46396b2dc27d5819a705eaf3e86
imgopz <rrgkmech@gmail.com>
2024-08-10 11:49:22 +0530

    adding root layout for navbar

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,30 @@ import DashboardPage from "./routes/dashboardPage/DashboardPage";
 import ChatPage from "./routes/chatPage/ChatPage";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/dashboard",
+    element: <RootLayout />,
     children: [
-      { path: "/dashboard", element: <DashboardPage /> },
-      { path: "/dashboard/chats/:id", element: <ChatPage /> },
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ],
+      },
     ],
   },
 ]);
